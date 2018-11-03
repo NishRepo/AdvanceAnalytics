79a455dd4a1a082c9170a7b25205f31c834581ca
nish <nishnit007@gmail.com>
2018-11-03 23:31:32 +0530

    commiting first code

---
+++ src/com/self/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.self.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("A new project to chekcin under GIT");
+	}
+
+}
